6fdf182b24d0f0fface5c3538dee93bdfebd0ba2
Arazim Project <144990276+arazonim@users.noreply.github.com>
2024-11-08 13:53:35 +0200

    fix bundle creation

--- scripts/create_bundle.py
+++ scripts/create_bundle.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     os.chdir(os.path.join(os.path.dirname(__file__), ".."))
     if os.path.exists("extension.zip"):
         os.remove("extension.zip")
-    os.system("zip -r extension.zip dist README.md manifest.json README.md")
+    os.system("zip -r extension.zip dist README.md manifest.json README.md icon")
